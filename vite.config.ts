707f54d17084d3bd7ef8b0a8f4adf7864bf83ba1
minxuanbo <mxb9127@gmail.com>
2026-08-07 14:53:44 +0800

    ci: add github actions auto deploy pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base路径必须写仓库名字！末尾带斜杠
+  base: '/personal-site/',
   plugins: [react()],
 })
